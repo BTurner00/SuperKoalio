8eb8f80f15830daee5872f9d04d3d4817b947e33
Ben <bhturne@g.clemson.edu>
2016-06-02 11:42:27 -0400

    Initial Commit

---
+++ desktop/src/com/theironyard/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.theironyard.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.theironyard.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+		config.height = 600;
+		config.width = 800;
+	}
+}
